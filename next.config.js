793d22fa47b4e9d0f2da0fc253146731d2f19b7e
Ted Kesgar <t.kesgar@live.com>
2020-04-13 06:00:00 +0700

    Remove unnecessary env parsing for next.config.js

--- next.config.js
+++ next.config.js
@@ -1,28 +1,4 @@
 /* eslint-env node */
-const dotenv = require("dotenv");
 const withSharo = require("@tkesgar/sharo-next");
 
-function loadEnv() {
-  dotenv.config();
-  dotenv.config({ path: `${process.env.NODE_ENV}.local.env` });
-  dotenv.config({ path: `${process.env.NODE_ENV}.env` });
-  dotenv.config({ path: "default.env" });
-}
-
-function getAppEnvs() {
-  const env = {};
-
-  for (const [key, value] of Object.entries(process.env)) {
-    if (key.startsWith("APP_")) {
-      env[key] = value;
-    }
-  }
-
-  return env;
-}
-
-loadEnv();
-
-module.exports = withSharo({
-  env: getAppEnvs(),
-});
+module.exports = withSharo();
