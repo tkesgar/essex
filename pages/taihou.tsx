0513a3c5db6f94040d61c8ba3e711521a4a80ac2
Ted Kesgar <t.kesgar@live.com>
2020-04-13 06:04:02 +0700

    Remove taihou page

--- pages/taihou.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-export default function Taihou(): JSX.Element {
-  React.useEffect(() => {
-    window.location.assign("https://youtu.be/dQw4w9WgXcQ?t=42");
-  }, []);
-
-  return null;
-}
